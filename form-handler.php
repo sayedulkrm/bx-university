427a29fe517ac93662f1e500d4a4a4bbdfa31fdc
Sayedul Karim <sayedulkrm@gmail.com>
2023-03-21 17:16:08 +0600

    first commit

---
+++ form-handler.php
@@ -0,0 +1,29 @@
+<?php
+$name = $_POST['name'];
+$visitor-email = $_POST['email'];
+$subject = $_POST['subject'];
+$message = $_POST['message'];
+
+
+$email_from = 'info@yourwebsite.com';
+
+$email_subject = 'New From Submission';
+
+$email_body = "User Name: $name.\n".
+            "User Email: $visitor_email.\n".
+            "User Subject: $subject.\n".
+            "User Message: $message.\n";
+
+
+$to = 'sayedulkrm2@gmail.com';
+
+$headers = "From: $email_from \r\n";
+$headers .="Reply-To: $visitor_email \r\n";
+
+mail($to,$email_subject,$email_body,$headers);
+
+header("Location: contact.html");
+
+
+
+?>
